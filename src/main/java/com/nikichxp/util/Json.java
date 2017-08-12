6ec9a09c70811c072dda96ece612f0d1fa648123
NikichXP <nikichx2@gmail.com>
2017-08-12 19:58:22 +0300

    possibility to get JSON array

--- src/main/java/com/nikichxp/util/Json.java
+++ src/main/java/com/nikichxp/util/Json.java
@@ -1,12 +1,14 @@
 package com.nikichxp.util;
 
+import lombok.AllArgsConstructor;
+
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Json {
 	
 	private HashMap<String, Object> data = new HashMap<>();
-		
+	
 	public static Json of (String key, Object... value) {
 		return new Json().and(key, value);
 	}
@@ -14,6 +16,10 @@ public class Json {
 	public Json and (String key, Object... value) {
 		data.put(key, value.length == 1 ? value[0] : value.length == 0 ? "" : new JsonArr(value));
 		return this;
+	}
+	
+	public JsonArr arr(Object... value) {
+		return new JsonArr(value);
 	}
 	
 	public String json () {
@@ -29,8 +35,8 @@ public class Json {
 		return json();
 	}
 	
-	@lombok.AllArgsConstructor
-	private static class JsonArr {
+	@AllArgsConstructor
+	public static class JsonArr {
 		private Object[] value;
 		
 		public String toString () {
