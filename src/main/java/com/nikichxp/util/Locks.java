bc1af109fc558871a88d8e1011dc751bd9a05f31
NikichXP <nikichx2@gmail.com>
2017-09-16 03:37:58 +0300

    Semaphores in Locks

--- src/main/java/com/nikichxp/util/Locks.java
+++ src/main/java/com/nikichxp/util/Locks.java
@@ -1,13 +1,18 @@
 package com.nikichxp.util;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
 
+/**
+ * Contains multithreading locks.
+ * Purpose: less lines of code.
+ */
 public class Locks {
 	
-	private static ConcurrentHashMap<Object, Object> locks = new ConcurrentHashMap<>();
+	private static ConcurrentHashMap<Object, Semaphore> locks = new ConcurrentHashMap<>();
 	
-	public static Object get (Object key) {
-		locks.putIfAbsent(key, new Object());
+	public static Object get (Object key, int... defaultValue) {
+		locks.putIfAbsent(key, new Semaphore((defaultValue.length == 0) ? 1 : defaultValue[0]));
 		return locks.get(key);
 	}
 	
@@ -15,6 +20,34 @@ public class Locks {
 		synchronized (get(a)) {
 			o.run();
 		}
+	}
+	
+	public void lock (Object key) {
+		Semaphore s = locks.get(key);
+		if (s == null) {
+			s = locks.put(key, new Semaphore(1));
+		}
+		try {
+			s.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void lock (Object key, int defaultValue) {
+		Semaphore s = locks.get(key);
+		if (s == null) {
+			s = locks.put(key, new Semaphore(defaultValue));
+		}
+		try {
+			s.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void unlock (Object key) {
+		locks.get(key).release();
 	}
 	
 }
